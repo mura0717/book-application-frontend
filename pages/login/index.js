e3103bc8a058a4dbd8c0f0c438f5eb1b13b1320c
mura0717 <mura0717@stud.kea.dk>
2023-05-03 22:59:55 +0200

    sign up & login commit 1

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,1 +1,25 @@
-export const initLogin = () => {};
+import fetchClient from "../../Fetcher.js";
+
+export const initLogin = () => {
+
+    const form = document.querySelector("form");
+    form.onsubmit = fetchInputValues;
+};
+
+async function fetchInputValues(e) {
+    
+    e.preventDefault();
+
+    const usernameInput = document.getElementById("input-username").value;
+    const passwordInput = document.getElementById("input-password").value;
+
+    const body = {
+        email: usernameInput,
+        password: passwordInput,
+    };
+  
+    const res = await fetchClient.postWithAuth("auth/login", body);
+    window.router.navigate("/booklist");
+
+
+};
--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,1 +1,27 @@
-export const initSignup = () => {};
+import fetchClient from "../../Fetcher.js";
+
+export const initSignup = () => {
+
+    const form = document.querySelector("form");
+    form.onsubmit = fetchInputValues;
+};
+
+async function fetchInputValues(e) {
+    
+    e.preventDefault();
+
+    const usernameInput = document.getElementById("input-username").value;
+    const emailInput = document.getElementById("input-email").value;
+    const passwordInput = document.getElementById("input-password").value;
+
+    const body = {
+        username: usernameInput,
+        email: emailInput,
+        password: passwordInput,
+    };
+  
+    const res = await fetchClient.post("/user-with-role", body);
+    window.router.navigate("/login");
+
+
+};
