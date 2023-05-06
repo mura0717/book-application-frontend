5348fd603340b9b4aa57bfb0780b4458485aad64
mura0717 <mura0717@stud.kea.dk>
2023-05-06 13:04:16 +0200

    booklists bootstrap integration part 1

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -3,25 +3,27 @@ import {fetchClient} from "../../utils.js";
 export const initSignup = () => {
 
     const form = document.querySelector("form");
-    form.onsubmit = fetchInputValues;
+    form.onsubmit = handleSignUp;
 };
 
-async function fetchInputValues(e) {
+async function handleSignUp(e) {
     
     e.preventDefault();
 
-    const usernameInput = document.getElementById("input-username").value;
-    const emailInput = document.getElementById("input-email").value;
-    const passwordInput = document.getElementById("input-password").value;
-
-    const body = {
-        username: usernameInput,
-        email: emailInput,
-        password: passwordInput,
-    };
+    const body = fetchInputValues();
   
     const res = await fetchClient.post("/user-with-role", body);
     window.router.navigate("/login");
 
+};
 
+async function fetchInputValues() {
+
+    const usernameInput = document.getElementById("input-username").value;
+    const passwordInput = document.getElementById("input-password").value;
+
+    return {
+        username: usernameInput,
+        password: passwordInput,
+    };
 };
