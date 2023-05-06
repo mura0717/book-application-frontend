0001e6a7c19cf4254a258e98e4b309a4bd341ea2
Cannabis2013 <MH2012@outlook.dk>
2023-05-07 00:43:29 +0200

    Refactored index.js a little

--- pages/book/index.js
+++ pages/book/index.js
@@ -6,17 +6,24 @@ import {setupBookDetails} from "./js/pageSetup/bookDetails.js";
 import {setupRecommendations} from "./js/pageSetup/recommendedBooks.js";
 
 export const initBook = (referenceId) => {
-    init(referenceId).then(() => {
-        setupFav(referenceId)
-        setupReviewDetails()
-        setupBookDetails()
-      }).then(() => {
-            Books.fetchRecommendations()
-              .then(() => setupRecommendations())
-    })
+    init(referenceId)
+        .then(initBookDetails)
+        .then(initRecommendations)
 };
 
 const init = async bookReference => {
     await Books.fetchBookDetails(bookReference)
     await BookLists.fetchBookLists()
+    return bookReference
+}
+
+const initBookDetails = referenceId => {
+    setupBookDetails()
+    setupFav(referenceId)
+    setupReviewDetails()
+}
+
+const initRecommendations = async () => {
+    await Books.fetchRecommendations()
+    setupRecommendations()
 }
