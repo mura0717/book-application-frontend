599d7244ec0d095d1dcbb62849ccc06b26f96f1c
mura0717 <mura0717@stud.kea.dk>
2023-05-06 14:05:33 +0200

    booklists phase2

--- pages/booklists/index.js
+++ pages/booklists/index.js
@@ -1,1 +1,53 @@
-export const initBookLists = () => {};
+import * as Factory from '../../shared/factories/elementFactory.js';
+import * as Booklists from '../../shared/bookLists/userBookLists.js';
+
+
+export const initBookLists = () => {
+
+    const el = Factory.createDivWithText("id","classNames", "text");
+
+    Booklists.fetchBookLists().then(setUpBookLists)
+};
+
+function setUpBookLists (){
+
+    const populatedListsElement = document.getElementById("booklists-id");
+    const booklists = Booklists.getBookLists();
+
+    for (let i = 0; i < booklists.length; i++) {
+        const booklist = booklists.at(i);
+        const listElement = createListElement(booklist);
+        populatedListsElement.appendChild(listElement);
+    }
+
+}
+
+function createListElement (bookList){
+    return "test"
+}
+
+/* export const initBook = (referenceId) => {
+    init(referenceId).then(() => {
+        setupFav(referenceId)
+        setupReviewDetails()
+        setupBookDetails()
+      }).then(() => {
+            Books.fetchRecommendations()
+              .then(() => setupRecommendations())
+    })
+};
+
+const init = async bookReference => {
+    await Books.fetchBookDetails(bookReference)
+    await BookLists.fetchBookLists()
+} 
+
+<li class="list-group-item d-flex justify-content-between align-items-start">
+          <div class="ms-2 me-auto">
+            <div class="fw-bold">Subheading</div>
+            Cras justo odio
+          </div>
+          <span class="badge bg-primary rounded-pill">14</span>
+        </li>
+
+*/
