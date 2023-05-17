1d4943675cf44b5871be38fbc3404fe933e7ad50
mura0717 <mura0717@stud.kea.dk>
2023-05-17 10:42:36 +0200

    deployment url

--- models/apiInfo.js
+++ models/apiInfo.js
@@ -2,4 +2,5 @@
     This value should be changed during deployment
  */
 
-export const baseURL = "http://localhost:8080/api"
+//export const baseURL = "http://localhost:8080/api"
+export const baseURL = "https://book-app-backend.azurewebsites.net/api" //deployment
